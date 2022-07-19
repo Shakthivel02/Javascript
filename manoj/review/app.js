6ae76a3724d2c3fd41e0b93c161c53e9a8f02008
Your Name <you@example.com>
2022-07-19 18:11:14 +0530

    a

---
+++ manoj/review/app.js
@@ -0,0 +1,32 @@
+//fibnaci
+function fibno(n) {
+    fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
+
+console.log(fibno(4));
+//factorial
+function Fact(n) {
+    let fabb = 1;
+    for (let i = 1; i <= n; i++) {
+        fabb = fabb * i;
+    }
+    return fabb;
+}
+
+console.log(Fact(10));
+//primeNumber
+function isPrime(n) {
+    if (n < 2) {
+        return false;
+    }
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0 ) {
+            return false;
+        } else return true;
+    }
+}
+console.log(isPrime(1)); 
