61a0dcb380b03b6f036244240d5107b81687c6fc
Shakthivel24 <shakthishanmugam04@gmail.com>
2022-07-20 11:48:33 +0530

    prime number

--- shakthi/algoritham/algo.js
+++ shakthi/algoritham/algo.js
@@ -17,4 +17,51 @@ const Fibonacci = (arg) => {
   }
 };
 
-//Fibonacci(3);
+Fibonacci(3);
+
+//primeNumber
+const Prime = (arg) => {
+  let isPrime;
+  if (arg === 1) {
+    console.log("1 is nor prime neither compound");
+  } else {
+    for (let i = 2; i <= Math.sqrt(arg); i++) {
+      if (arg % i === 0) {
+        isPrime = false;
+        break;
+      } else {
+        isPrime = true;
+      }
+    }
+  }
+  return isPrime;
+};
+
+//console.log(Prime(7) ? "its a prime number" : "its not a prime number");
+
+//power of 2:
+
+const isPowerOfTwo = (arg) => {
+  do {
+    if (arg % 2 !== 0) {
+      return false;
+    } else {
+      arg /= 2;
+    }
+  } while (arg > 1);
+  return true;
+};
+
+//console.log(isPowerOfTwo(7));
+
+//method 2:
+//only works for 1,2,4,8,...
+
+const ispower = (n) => {
+  if (n < 1) {
+    return false;
+  }
+  return (n & (n - 1)) === 0;
+};
+
+console.log(ispower(16));
